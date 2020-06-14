7eee681fc9c09b4202748b224c6ebc511b439519
Hitendra <Hitendra@Hitendra-PC>
2020-06-14 18:05:17 +0530

    Second Commit

--- WebDriverManagerDemo/src/test/java/com/test/WebDriverManagerDemo.java
+++ WebDriverManagerDemo/src/test/java/com/test/WebDriverManagerDemo.java
@@ -1,6 +1,8 @@
 package com.test;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,34 +12,35 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WebDriverManagerDemo {
 	WebDriver driver;
-	
+
 	@BeforeMethod
 	public void setup() {
-		//WebDriverManager.chromedriver().setup();
-		//WebDriverManager.chromedriver().driverVersion("84.0.4147.30").setup();
-		//driver = new ChromeDriver();
-		
-		//WebDriverManager.firefoxdriver().setup();
-		//driver= new FirefoxDriver();
-		
-		WebDriverManager.iedriver().setup();
-		driver = new InternetExplorerDriver();
-		
+
+		if (System.getProperty("browser").equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else if (System.getProperty("browser").equalsIgnoreCase("IE")) {
+			WebDriverManager.iedriver().setup();
+			driver = new InternetExplorerDriver();
+
+		}
 		driver.manage().window().maximize();
 	}
 
 	@Test
 	public void webDriverManagerTest() {
-		
-		driver.get("https://www.automationtestinginsider.com/");
-		String titleString =driver.getTitle();
-		System.out.println("Page Title is: "+titleString);
-		
+
+		driver.get(System.getProperty("url"));
+		String titleString = driver.getTitle();
+		System.out.println("Page Title is: " + titleString);
+
 	}
-	
+
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
 	}
 }
-
